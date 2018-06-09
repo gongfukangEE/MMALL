c620f2041f723790370f91f2d99a4cbb663f0217
gongfukangEE <gongfukang@outlook.com>
2018-06-09 20:00:42 +0800

    FTP 配置富文本上传文件

--- src/main/java/com/mmall/service/IFileService.java
+++ src/main/java/com/mmall/service/IFileService.java
@@ -1,8 +1,12 @@
 package com.mmall.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  * @Auther gongfukang
  * @Date 2018/6/8 16:20
  */
 public interface IFileService {
+
+    String upload(MultipartFile file, String path);
 }
--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -1,8 +1,11 @@
 package com.mmall.service.impl;
 
+import com.google.common.collect.Lists;
 import com.mmall.service.IFileService;
+import com.mmall.util.FTPUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -13,10 +16,12 @@ import java.util.UUID;
  * @Auther gongfukang
  * @Date 2018/6/8 16:22
  */
+@Service("iFileService")
 public class FileServiceImpl implements IFileService {
 
     private Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
 
+    @Override
     public String upload(MultipartFile file, String path) {
         String fileName = file.getOriginalFilename();
         // 扩展名 abc.jpg
@@ -36,9 +41,10 @@ public class FileServiceImpl implements IFileService {
         try {
             //文件上传成功
             file.transferTo(targetFile);
-
-            //将 targetFile 上传到 FTP，然后删除 upload 下面的文件
-
+            //已经上传到 FTP 服务器上
+            FTPUtil.uploadFile(Lists.<File>newArrayList(targetFile));
+            //删除 upload 下面的文件
+            targetFile.delete();
         } catch (IOException e) {
             logger.error("上传文件异常", e);
             return null;

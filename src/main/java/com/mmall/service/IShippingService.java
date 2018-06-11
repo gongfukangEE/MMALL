5747fa8b28926450cb6387915edc1f2ed3d5afbe
gongfukangEE <gongfukang@outlook.com>
2018-06-11 17:13:12 +0800

    地址管理模块
    增删改查+列表页面

---
+++ src/main/java/com/mmall/service/IShippingService.java
@@ -0,0 +1,22 @@
+package com.mmall.service;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Shipping;
+
+/**
+ * @Auther gongfukang
+ * @Date 2018/6/11 15:52
+ */
+public interface IShippingService {
+
+    ServerResponse add(Integer userId, Shipping shipping);
+
+    ServerResponse<String> del(Integer userId, Integer shippingId);
+
+    ServerResponse update(Integer userId, Shipping shipping);
+
+    ServerResponse<Shipping> select(Integer userId, Integer shippingId);
+
+    ServerResponse<PageInfo> list(Integer userId, int pageNum, int pageSize);
+}
---
+++ src/main/java/com/mmall/service/impl/ShippingServiceImpl.java
@@ -0,0 +1,77 @@
+package com.mmall.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.google.common.collect.Maps;
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.ShippingMapper;
+import com.mmall.pojo.Shipping;
+import com.mmall.service.IShippingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Auther gongfukang
+ * @Date 2018/6/11 15:52
+ */
+@Service("iShippingService")
+public class ShippingServiceImpl implements IShippingService {
+
+    @Autowired
+    ShippingMapper shippingMapper;
+
+    @Override
+    public ServerResponse add(Integer userId, Shipping shipping) {
+        shipping.setUserId(userId);
+        // id 填充到 shipping.getId 返回前端
+        int rowCount = shippingMapper.insert(shipping);
+        if (rowCount > 0) {
+            Map result = Maps.newHashMap();
+            result.put("shippingId", shipping.getId());
+            return ServerResponse.createBySuccess("新建地址成功", result);
+        }
+        return ServerResponse.createByErrorMessage("新建地址失败");
+    }
+
+    @Override
+    public ServerResponse<String> del(Integer userId, Integer shippingId) {
+        int resultCount = shippingMapper.deleteByPrimaryKey(shippingId);
+        if (resultCount > 0) {
+            return ServerResponse.createBySuccess("删除地址成功");
+        }
+        return ServerResponse.createByErrorMessage("删除地址失败");
+    }
+
+    @Override
+    public ServerResponse update(Integer userId, Shipping shipping) {
+        shipping.setUserId(userId);
+        int rowCount = shippingMapper.updateByShipping(shipping);
+        if (rowCount > 0) {
+            return ServerResponse.createBySuccess("更新地址成功");
+        }
+        return ServerResponse.createByErrorMessage("更新地址失败");
+    }
+
+    @Override
+    public ServerResponse<Shipping> select(Integer userId, Integer shippingId) {
+        Shipping shipping = shippingMapper.selectByShippingIdUserId(userId, shippingId);
+        if (shipping == null) {
+            return ServerResponse.createByErrorMessage("无法查询到改地址");
+        }
+        return ServerResponse.createBySuccess("查询地址成功", shipping);
+    }
+
+    /**
+     * 分页
+     */
+    @Override
+    public ServerResponse<PageInfo> list(Integer userId, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<Shipping> shippingList = shippingMapper.selectByUserId(userId);
+        PageInfo pageInfo = new PageInfo(shippingList);
+        return ServerResponse.createBySuccess(pageInfo);
+    }
+}

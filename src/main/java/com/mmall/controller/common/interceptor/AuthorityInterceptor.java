f52899e87f085b94dff90d2322c85eb502dc1352
gongfukangEE <gongfukang@outlook.com>
2018-07-08 16:05:00 +0800

    拦截器权限管理配置

---
+++ src/main/java/com/mmall/controller/common/interceptor/AuthorityInterceptor.java
@@ -0,0 +1,29 @@
+package com.mmall.controller.common.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Auther gongfukang
+ * @Date 7/8 16:02
+ * 拦截器
+ */
+public class AuthorityInterceptor implements HandlerInterceptor{
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}

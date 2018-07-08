46bc8b337fce10f7f1204e4f3688cb25ac06e881
gongfukangEE <gongfukang@outlook.com>
2018-07-08 21:51:18 +0800

    解决登陆拦截问题

--- src/main/java/com/mmall/controller/backend/UserManagerController.java
+++ src/main/java/com/mmall/controller/backend/UserManagerController.java
@@ -1,15 +1,20 @@
 package com.mmall.controller.backend;
 
 import com.mmall.common.Const;
+import com.mmall.common.RedisShardedPool;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisShardedPoolUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -22,15 +27,17 @@ public class UserManagerController {
     @Autowired
     private IUserService iUserService;
 
-    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @RequestMapping(value = "login.do", method = RequestMethod.GET)
     @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session) {
-        ServerResponse<User> response = iUserService.login(username,password);
+    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse httpServletResponse) {
+        ServerResponse<User> response = iUserService.login(username, password);
         if (response.isSuccess()) {
             User user = response.getDate();
             if (user.getRole() == Const.Role.ROLE_ADMIN) {
-                // 说明登陆的管理员
-                session.setAttribute(Const.CURRENT_USER, user);
+                // 说明登陆的管理员，改为 Redis 共享 session 的方式
+                //session.setAttribute(Const.CURRENT_USER, user);
+                CookieUtil.writeLoginToken(httpServletResponse, session.getId());
+                RedisShardedPoolUtil.setEx(session.getId(), Const.RedisCacheExtime.REDIS_SESSION_EXTIME, JsonUtil.obj2String(response.getDate()));
                 return response;
             } else {
                 return ServerResponse.createByErrorMessage("不是管理员，无法登陆");
--- src/main/java/com/mmall/controller/common/interceptor/AuthorityInterceptor.java
+++ src/main/java/com/mmall/controller/common/interceptor/AuthorityInterceptor.java
@@ -1,29 +1,119 @@
 package com.mmall.controller.common.interceptor;
 
+import com.google.common.collect.Maps;
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisShardedPoolUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * @Auther gongfukang
  * @Date 7/8 16:02
  * 拦截器
  */
-public class AuthorityInterceptor implements HandlerInterceptor{
+@Slf4j
+public class AuthorityInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return false;
+        log.info("preHandle");
+
+        // 请求中的方法名字
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+
+        // 解析 HandlerMethod
+        String methodName = handlerMethod.getMethod().getName();
+        String className = handlerMethod.getBean().getClass().getSimpleName();
+
+        // 解析参数，具体的参数 key value 是什么，日志打印
+        StringBuffer requestParamBuffer = new StringBuffer();
+        Map paramMap = request.getParameterMap();
+        Iterator it = paramMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry entry = (Map.Entry) it.next();
+            String mapKey = (String) entry.getKey();
+            String mapValue = StringUtils.EMPTY;
+
+            // request 这个参数的 map 里面的 value 返回的是一个 String[]
+            Object obj = entry.getValue();
+            if (obj instanceof String[]) {
+                String[] strs = (String[]) obj;
+                mapValue = Arrays.toString(strs);
+            }
+            requestParamBuffer.append(mapKey).append("=").append(mapValue);
+        }
+
+        if (StringUtils.equals(className, "UserManagerController") && StringUtils.equals(methodName, "login")) {
+            log.info("权限拦截器拦截的请求，className:{}, methodName:{}", className, methodName);
+            // 如果是拦截的登陆请求，不打印参数，参数中的密码会打印到日志中，不安全
+            return true;
+        }
+
+        // 获取用户
+        User user = null;
+        String loginToken = CookieUtil.readLoginToken(request);
+        if (StringUtils.isNotEmpty(loginToken)) {
+            String userJsonStr = RedisShardedPoolUtil.get(loginToken);
+            user = JsonUtil.string2Obj(userJsonStr, User.class);
+        }
+
+        // response 托管到 拦截器当中，将 dispatcher-servlet 中与 json 相关的配置全部重写
+        if (user == null || (user.getRole().intValue() != Const.Role.ROLE_ADMIN)) {
+            // 返回 false，即不会调用 Controller 中的方法
+            response.reset();       // 这里要添加 reset，否则要报异常
+            response.setCharacterEncoding("UTF-8");     //这里要设置编码，否则乱码
+            response.setContentType("application/json;charset=UTF-8");      //设置返回值的类型，因为全部是 json 接口
+
+            PrintWriter out = response.getWriter();
+
+            if (user == null) {
+                // 富文本上传处理
+                if (StringUtils.equals(className, "ProductManageController") && StringUtils.equals(methodName, "richtextImgUpload")) {
+                    Map resultMap = Maps.newHashMap();
+                    resultMap.put("success", false);
+                    resultMap.put("msg", "请登陆管理员");
+                    out.print(JsonUtil.obj2String(resultMap));
+                } else {
+                    out.print(JsonUtil.obj2String(ServerResponse.createByErrorMessage("拦截器拦截，用户未登录")));
+                }
+            } else {
+                if (StringUtils.equals(className, "ProductManageController") && StringUtils.equals(methodName, "richtextImgUpload")) {
+                    Map resultMap = Maps.newHashMap();
+                    resultMap.put("success", false);
+                    resultMap.put("msg", "无权限操作");
+                    out.print(JsonUtil.obj2String(resultMap));
+                } else {
+                    out.print(JsonUtil.obj2String(ServerResponse.createByErrorMessage("拦截器拦截，用户无权限操作")));
+                }
+            }
+            out.flush();
+            out.close();
+
+            return false;
+        }
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
+        log.info("postHandle");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
+        log.info("afterCompletion");
     }
 }

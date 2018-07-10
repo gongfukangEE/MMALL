352d6d1d86b44814c926c826775288583663c23f
gongfukangEE <gongfukang@outlook.com>
2018-07-10 17:23:35 +0800

    git test

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -59,4 +59,6 @@ public class CloseOrderTask {
         log.info("释放: {}, ThreadName: {}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, Thread.currentThread().getName());
         log.info("--------------------------------------------------------------------------------------------------");
     }
+
+    // test
 }
